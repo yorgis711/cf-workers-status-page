d0eb9bda2ecb4ab3edc1bd6885ffc40497c65ccb
yorgis711 <105851696+yorgis711@users.noreply.github.com>
2023-02-25 13:24:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,14 +27,7 @@ addEventListener('fetch', (event) => {
   }
 })
 
-  async fetch(request, env) {
-    env.WEATHER.writeDataPoint({
-      'blobs': ["Seattle", "USA", "pro_sensor_9000"],
-      'doubles': [25, 0.5],
-      'indexes': ["a3cd45"] // Sensor ID
-    });
-    return new Response("OK!");
-  }
+
 
 addEventListener('scheduled', (event) => {
   event.waitUntil(processCronTrigger(event))
